350c1d3c1346a3c26d328df7769adce284e1578d
nstratos <devnull@localhost>
2015-01-10 01:28:50 +0200

    Added status bar and moved most draw functions to draw packet

--- draw/draw.go
+++ draw/draw.go
@@ -1,34 +1,176 @@
 package draw
 
-import "github.com/nsf/termbox-go"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
 
-const edit_box_width = 50
+	"github.com/nsf/termbox-go"
+)
 
-func Box() {
+var Labels = map[rune]string{
+	'a': "Visual imagination",
+	'd': "Language thought",
+	'e': "Language voice",
+	'q': "Visual memory",
+	's': "Auditory imagination",
+	'w': "Auditory memory",
+}
+
+const title = `           _ _   
+  _ __  __| | |_ 
+ | '  \/ _  |  _|
+ |_|_|_\__,_|\__|
+                 
+`
+
+func Title() (maxX, maxY int) {
+	return Text(0, 0, title)
+}
+
+type Capture struct {
+	Value   rune
+	Seconds int
+	Hz      float64
+}
+
+func (c *Capture) Label() string {
+	return Labels[c.Value]
+}
+
+func (c *Capture) Timestamp() string {
+	return formatTimer(c.Seconds)
+}
+
+func formatTimer(seconds int) string {
+	min := seconds / 60
+	sec := seconds % 60
+	m := fmt.Sprintf("%v", min)
+	if min < 10 {
+		m = fmt.Sprintf("0%v", min)
+	}
+	s := fmt.Sprintf("%v", sec)
+	if sec < 10 {
+		s = fmt.Sprintf("0%v", sec)
+	}
+	return fmt.Sprintf("%v:%v", m, s)
+}
+
+func cells() [][]termbox.Cell {
+	mx, my := termbox.Size()
+	cells := termbox.CellBuffer()
+	cells2d := make([][]termbox.Cell, mx)
+	for k := range cells2d {
+		cells2d[k] = make([]termbox.Cell, my)
+	}
+	i, j := 0, 0
+	for _, c := range cells {
+		if i == mx {
+			j += 1
+			i = 0
+		}
+		cells2d[i][j] = c
+		i += 1
+	}
+	return cells2d
+}
+
+func GetCell(x, y int) termbox.Cell {
+	c := cells()
+	return c[x][y]
+}
+
+func GetInput(startX, endX, y int) string {
+	c := cells()
+	//l := endY - startY
+	runes := make([]rune, 0)
+	//i := 0
+	for x := startX; x <= endX; x++ {
+		//runes[i] = c[x][y].Ch
+		//i += 1
+		runes = append(runes, c[x][y].Ch)
+	}
+	return string(runes)
+}
+
+// func Box() {
+// 	const coldef = termbox.ColorDefault
+// 	//termbox.Clear(coldef, coldef)
+// 	_, h := termbox.Size()
+
+// 	midy := h - 2
+// 	//midx := (w - edit_box_width) / 2
+// 	midx := 1
+
+// 	// unicode box drawing chars around the edit box
+// 	termbox.SetCell(midx-1, midy, '│', coldef, coldef)
+// 	termbox.SetCell(midx+edit_box_width, midy, '│', coldef, coldef)
+// 	termbox.SetCell(midx-1, midy-1, '╭', coldef, coldef)
+// 	termbox.SetCell(midx-1, midy+1, '╰', coldef, coldef)
+// 	termbox.SetCell(midx+edit_box_width, midy-1, '╮', coldef, coldef)
+// 	termbox.SetCell(midx+edit_box_width, midy+1, '╯', coldef, coldef)
+// 	fill(midx, midy-1, edit_box_width, 1, termbox.Cell{Ch: '─'})
+// 	fill(midx, midy+1, edit_box_width, 1, termbox.Cell{Ch: '─'})
+
+// 	//edit_box.Draw(midx, midy, edit_box_width, 1)
+// 	//termbox.SetCursor(midx+edit_box.CursorX(), midy)
+// 	termbox.SetCursor(midx, midy)
+
+// 	tbprint(midx+6, midy+3, coldef, coldef, "Press ESC to quit")
+// 	//termbox.Flush()
+// }
+
+type StatusBar struct {
+	X          int
+	Y          int
+	Width      int
+	Text       string
+	timerWidth int
+}
+
+func NewStatusBar(x, y, width int, text string) *StatusBar {
+	return &StatusBar{x, y, width, text, 6}
+}
+
+func (sb StatusBar) Draw() {
 	const coldef = termbox.ColorDefault
-	//termbox.Clear(coldef, coldef)
-	_, h := termbox.Size()
-
-	midy := h - 2
-	//midx := (w - edit_box_width) / 2
-	midx := 1
+	x := sb.X
+	y := sb.Y
+	width := sb.Width
+	text := sb.Text
+	timerWidth := sb.timerWidth
 
 	// unicode box drawing chars around the edit box
-	termbox.SetCell(midx-1, midy, '│', coldef, coldef)
-	termbox.SetCell(midx+edit_box_width, midy, '│', coldef, coldef)
-	termbox.SetCell(midx-1, midy-1, '┌', coldef, coldef)
-	termbox.SetCell(midx-1, midy+1, '└', coldef, coldef)
-	termbox.SetCell(midx+edit_box_width, midy-1, '┐', coldef, coldef)
-	termbox.SetCell(midx+edit_box_width, midy+1, '┘', coldef, coldef)
-	fill(midx, midy-1, edit_box_width, 1, termbox.Cell{Ch: '─'})
-	fill(midx, midy+1, edit_box_width, 1, termbox.Cell{Ch: '─'})
+	termbox.SetCell(x, y+0, '╔', coldef, coldef)
+	termbox.SetCell(x, y+1, '║', coldef, coldef)
+	termbox.SetCell(x, y+2, '╚', coldef, coldef)
+	fill(x+1, y+0, timerWidth, 1, termbox.Cell{Ch: '═'})
+	fill(x+1, y+2, timerWidth, 1, termbox.Cell{Ch: '═'})
+	termbox.SetCell(x+timerWidth+1, y+0, '╤', coldef, coldef)
+	termbox.SetCell(x+timerWidth+1, y+1, '│', coldef, coldef)
+	termbox.SetCell(x+timerWidth+1, y+2, '╧', coldef, coldef)
+	fill(x+timerWidth+2, y+0, width, 1, termbox.Cell{Ch: '═'})
+	fill(x+timerWidth+2, y+2, width, 1, termbox.Cell{Ch: '═'})
+	termbox.SetCell(x+timerWidth+2+width, y+0, '╗', coldef, coldef)
+	termbox.SetCell(x+timerWidth+2+width, y+1, '║', coldef, coldef)
+	termbox.SetCell(x+timerWidth+2+width, y+2, '╝', coldef, coldef)
+	Text(x+timerWidth+2, y+1, text)
 
-	//edit_box.Draw(midx, midy, edit_box_width, 1)
-	//termbox.SetCursor(midx+edit_box.CursorX(), midy)
-	termbox.SetCursor(midx, midy)
+	termbox.Flush()
+}
 
-	tbprint(midx+6, midy+3, coldef, coldef, "Press ESC to quit")
-	//termbox.Flush()
+func (sb StatusBar) UpdateTimer(seconds int) {
+	Text(sb.X+1, sb.Y+1, formatTimer(seconds))
+	termbox.Flush()
+}
+
+func (sb StatusBar) UpdateText(text string) {
+	//Text(sb.X+sb.timerWidth+2, sb.Y+1, text[0:sb.Width]) // panics for some reason
+	fill(sb.X+sb.timerWidth+2, sb.Y+1, sb.Width, 1, termbox.Cell{Ch: ' '})
+	Text(sb.X+sb.timerWidth+2, sb.Y+1, text)
+	termbox.Flush()
 }
 
 func fill(x, y, w, h int, cell termbox.Cell) {
@@ -44,4 +186,93 @@ func tbprint(x, y int, fg, bg termbox.Attribute, msg string) {
 		termbox.SetCell(x, y, c, fg, bg)
 		x++
 	}
+}
+
+func Text(x, y int, s string) (maxX, maxY int) {
+	mx := 0
+	tempx := x
+	text := []string{s}
+	if strings.Contains(s, "\n") {
+		text = strings.Split(s, "\n")
+	}
+	for _, t := range text {
+		for _, r := range t {
+			termbox.SetCell(x, y, r, termbox.ColorDefault, termbox.ColorDefault)
+			x += 1
+		}
+		if x > mx {
+			mx = x
+		}
+		x = tempx
+		y += 1
+	}
+	// Because we always icrease it one more.
+	y -= 1
+	return mx, y
+}
+
+func DrawOptions(x, y int, c Config) (finalX, finalY int) {
+
+	_, y = Text(x, y, "───────────────")
+	_, y = Text(x, y+1, "Mode: "+strconv.QuoteRuneToASCII(c.Mode))
+	_, y = Text(x, y+1, "───────────────")
+	_, y = Text(x, y+1, "TotalTime: "+strconv.Itoa(c.TotalTime)+" min")
+	_, y = Text(x, y+1, "───────────────")
+	_, y = Text(x, y+1, "Offset: "+strconv.Itoa(c.Offset))
+	_, y = Text(x, y+1, "───────────────")
+	_, y = Text(x, y+1, fmt.Sprintf("Base: %.2f hz", c.BaseHz))
+	_, y = Text(x, y+1, "───────────────")
+	_, y = Text(x, y+1, fmt.Sprintf("Start: %.2f hz", c.StartHz))
+	_, y = Text(x, y+1, "───────────────")
+	_, y = Text(x, y+1, fmt.Sprintf("End: %.2f hz", c.EndHz))
+	_, y = Text(x, y+1, "───────────────")
+	return x, y
+}
+
+func DrawKeyLabels(x, y int) (finalX, finalY int) {
+	_, y = Text(x, y, fmt.Sprintf("%v = %v", strconv.QuoteRuneToASCII('q'), Labels['q']))
+	_, y = Text(x, y+1, fmt.Sprintf("%v = %v", strconv.QuoteRuneToASCII('a'), Labels['a']))
+	_, y = Text(x, y+1, fmt.Sprintf("%v = %v", strconv.QuoteRuneToASCII('w'), Labels['w']))
+	_, y = Text(x, y+1, fmt.Sprintf("%v = %v", strconv.QuoteRuneToASCII('s'), Labels['s']))
+	_, y = Text(x, y+1, fmt.Sprintf("%v = %v", strconv.QuoteRuneToASCII('e'), Labels['e']))
+	_, y = Text(x, y+1, fmt.Sprintf("%v = %v", strconv.QuoteRuneToASCII('d'), Labels['d']))
+	return x, y
+}
+
+type Config struct {
+	Mode      rune
+	TotalTime int
+	Offset    int
+	BaseHz    float64
+	StartHz   float64
+	EndHz     float64
+}
+
+func (c Config) Save() error {
+	json, err := json.MarshalIndent(c, "", "  ")
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile("config.json", json, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Config) Load() error {
+	b, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(b, c)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// CurrentHz = Seconds * (EndHz - StartHz) / ((TotalTime - Offset) * 60)
+func CurrentHz(seconds int, c Config) float64 {
+	return (float64(seconds) * (c.EndHz - c.StartHz) / float64((c.TotalTime-c.Offset)*60)) + c.StartHz
 }
--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"strconv"
-	"strings"
 	"time"
 
 	"bitbucket.org/nstratos/mdt/draw"
@@ -14,95 +11,15 @@ import (
 	"github.com/nsf/termbox-go"
 )
 
-const title = `
-      ___           ___           ___     
-     /\__\         /\  \         /\  \    
-    /::|  |       /::\  \        \:\  \   
-   /:|:|  |      /:/\:\  \        \:\  \  
-  /:/|:|__|__   /:/  \:\__\       /::\  \ 
- /:/ |::::\__\ /:/__/ \:|__|     /:/\:\__\
- \/__/~~/:/  / \:\  \ /:/  /    /:/  \/__/
-       /:/  /   \:\  /:/  /    /:/  /     
-      /:/  /     \:\/:/  /     \/__/      
-     /:/  /       \::/__/                 
-     \/__/         ~~                     
-`
-
-type Config struct {
-	Mode      rune
-	TotalTime int
-	Offset    int
-	BaseHz    float64
-	StartHz   float64
-	EndHz     float64
-}
-
-func (c Config) Save() error {
-	json, err := json.MarshalIndent(c, "", "  ")
-	if err != nil {
-		return err
-	}
-	err = ioutil.WriteFile("config.json", json, 0644)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (c *Config) Load() error {
-	b, err := ioutil.ReadFile("config.json")
-	if err != nil {
-		return err
-	}
-	err = json.Unmarshal(b, c)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-var labels = map[rune]string{
-	'a': "Visual imagination",
-	'd': "Language thought",
-	'e': "Language voice",
-	'q': "Visual memory",
-	's': "Auditory imagination",
-	'w': "Auditory memory",
-}
-
-type Capture struct {
-	Value   rune
-	Seconds int
-	Hz      float64
-}
-
-func (c *Capture) Label() string {
-	return labels[c.Value]
-}
-
-func (c *Capture) Timestamp() string {
-	min := c.Seconds / 60
-	sec := c.Seconds % 60
-	m := fmt.Sprintf("%v", min)
-	if min < 10 {
-		m = fmt.Sprintf("0%v", min)
-	}
-	s := fmt.Sprintf("%v", sec)
-	if sec < 10 {
-		s = fmt.Sprintf("0%v", sec)
-	}
-	return fmt.Sprintf("%v:%v", m, s)
-}
-
 // Global holder of captured key presses.
-var captures = make([]Capture, 0)
-
-// Current line to print text.
-var line = 2
+var captures = make([]draw.Capture, 0)
 
 // Logs to .txt file in program's directory, named: S-E hz day date month time
 // where S is start hz and E is end hz, e.g. '15-19 hz wed 27 dec 22.09.txt'
-func logCaptures(c Config) error {
+func logCaptures(c draw.Config) error {
+	if len(captures) == 0 {
+		return nil
+	}
 	format := "Mon 02 Jan 15.04"
 	filename := fmt.Sprintf("%v-%v hz %v", c.StartHz, c.EndHz, time.Now().Format(format))
 	f, err := os.Create(filename + ".txt")
@@ -124,13 +41,13 @@ func logCaptures(c Config) error {
 	}
 	// Emptying capture holder.
 	captures = nil
-	captures = make([]Capture, 0)
+	captures = make([]draw.Capture, 0)
 	return nil
 }
 
 func main() {
 	// Loading configuration from config.json
-	c := Config{}
+	c := draw.Config{}
 	c.Load()
 
 	// Initializing termbox
@@ -146,12 +63,16 @@ func main() {
 
 	// Drawing title
 	x, y := 0, 0
-	_, y = printText(x, y, title)
-	_, y = printText(x, y+2, "Press 'space' to start capturing keys, 'Esc' to quit")
+	_, y = draw.Title()
+	mx, my := termbox.Size()
+	draw.Text(0, 0, fmt.Sprintf("%vx%v", mx, my))
+	//_, y = draw.Text(x, y+1, "Press 'space' to start capturing keys, 'Esc' to quit")
 	keepY := y
-	_, y = printOptions(x, y+2, c)
-	_, y = printKeyLabels(x+25, keepY+2)
-	draw.Box()
+	_, y = draw.DrawOptions(0, y+1, c)
+	sb := draw.NewStatusBar(0, y+1, 54, "Press 'space' to start capturing keys, 'Esc' to quit.")
+	sb.Draw()
+	_, y = draw.DrawKeyLabels(x+25, keepY+2)
+
 	termbox.Flush()
 
 	letter := make(chan rune)
@@ -171,7 +92,7 @@ loop:
 		case <-start:
 			capturing = !capturing
 			if capturing {
-				go timer(5, letter, endTimer, c)
+				go timer(5, letter, endTimer, c, *sb)
 				timerEnded = false
 			}
 			if !capturing && !timerEnded {
@@ -197,32 +118,28 @@ loop:
 	}
 }
 
-func timer(maxSeconds int, letter chan rune, end chan bool, c Config) {
+func timer(maxSeconds int, letter chan rune, end chan bool, c draw.Config, sb draw.StatusBar) {
 	seconds := 0
-	min, sec := 0, 0
+	// min, sec := 0, 0
 	expired := time.NewTimer(time.Second * time.Duration(maxSeconds)).C
 	tick := time.NewTicker(time.Second).C
+	sb.UpdateTimer(seconds)
 	for {
 		select {
 		case l := <-letter:
-			capture := Capture{Value: l, Seconds: seconds, Hz: currentHz(seconds, c)}
+			capture := draw.Capture{Value: l, Seconds: seconds, Hz: draw.CurrentHz(seconds, c)}
 			captures = append(captures, capture)
-			printText(0, 0, fmt.Sprintf("Recorded %v (%v)", strconv.QuoteRune(l), currentHz(seconds, c)))
-			termbox.Flush()
+			//printText(0, 0, fmt.Sprintf("Recorded %v (%v)", strconv.QuoteRune(l), currentHz(seconds, c)))
+			sb.UpdateText(fmt.Sprintf("Recorded %v (%.2fhz) \"%v\"", strconv.QuoteRune(l), draw.CurrentHz(seconds, c), draw.Labels[l]))
+			//termbox.Flush()
 		case <-end:
 			return
 		case <-expired:
 			end <- true
 			return
 		case <-tick:
-			seconds = seconds + 1
-			if sec == 59 {
-				sec = -1
-				min += 1
-			}
-			sec += 1
-			printText(0, 1, fmt.Sprintf("%d min %d sec", min, sec))
-			termbox.Flush()
+			seconds += 1
+			sb.UpdateTimer(seconds)
 		}
 	}
 }
@@ -240,69 +157,15 @@ func captureEvents(letter chan rune, start chan bool, done chan bool) {
 		case supportedLabel(ev.Ch):
 			letter <- ev.Ch
 		case ev.Type == termbox.EventMouse:
-			printText(0, 1, fmt.Sprintf("Mouse clicked %d, %d", ev.MouseX, ev.MouseY))
+			cell := draw.GetCell(ev.MouseX, ev.MouseY)
+			draw.Text(2, 22, fmt.Sprintf("Mouse clicked (%d, %d) = %v", ev.MouseX, ev.MouseY, strconv.QuoteRuneToASCII(cell.Ch)))
+			//input := draw.GetInput(ev.MouseX, ev.MouseX+3, ev.MouseY)
+			//printText(2, 22, fmt.Sprintf("Mouse clicked (%d, %d) = %v", ev.MouseX, ev.MouseY, input))
 			termbox.Flush()
+			// case ev.Key == termbox.KeyEnter:
+			// 	draw.Input()
 		}
 	}
-}
-
-// CurrentHz = Seconds * (EndHz - StartHz) / ((TotalTime - Offset) * 60)
-func currentHz(seconds int, c Config) float64 {
-	return (float64(seconds) * (c.EndHz - c.StartHz) / float64((c.TotalTime-c.Offset)*60)) + c.StartHz
-}
-
-func showResults(c Config) {
-	printText(0, 0, "RESULTS")
-	l := 1
-	for _, capt := range captures {
-		printText(0, l, fmt.Sprintf("%.2fhz @ %.0f base hz, on %v %v", capt.Hz, c.BaseHz, capt.Timestamp(), capt.Label()))
-		l += 1
-	}
-	termbox.Flush()
-}
-
-func printOptions(x, y int, c Config) (finalX, finalY int) {
-	x, y = printText(x, y, "Mode: "+strconv.QuoteRuneToASCII(c.Mode))
-	x, y = printText(x, y+1, "TotalTime: "+strconv.Itoa(c.TotalTime)+" min")
-	x, y = printText(x, y+1, "Offset: "+strconv.Itoa(c.Offset))
-	x, y = printText(x, y+1, fmt.Sprintf("Base: %.2f hz", c.BaseHz))
-	x, y = printText(x, y+1, fmt.Sprintf("Start: %.2f hz", c.StartHz))
-	x, y = printText(x, y+1, fmt.Sprintf("End: %.2f hz", c.EndHz))
-	return x, y
-}
-
-func printKeyLabels(x, y int) (finalX, finalY int) {
-	x, y = printText(x, y, fmt.Sprintf("%v = %v", strconv.QuoteRuneToASCII('q'), labels['q']))
-	x, y = printText(x, y+1, fmt.Sprintf("%v = %v", strconv.QuoteRuneToASCII('a'), labels['a']))
-	x, y = printText(x, y+1, fmt.Sprintf("%v = %v", strconv.QuoteRuneToASCII('w'), labels['w']))
-	x, y = printText(x, y+1, fmt.Sprintf("%v = %v", strconv.QuoteRuneToASCII('s'), labels['s']))
-	x, y = printText(x, y+1, fmt.Sprintf("%v = %v", strconv.QuoteRuneToASCII('e'), labels['e']))
-	x, y = printText(x, y+1, fmt.Sprintf("%v = %v", strconv.QuoteRuneToASCII('d'), labels['d']))
-	return x, y
-}
-
-func printText(x, y int, s string) (finalX, finalY int) {
-	tempx := x
-	text := []string{s}
-	if strings.Contains(s, "\n") {
-		text = strings.Split(s, "\n")
-	}
-	for _, t := range text {
-		for _, r := range t {
-			termbox.SetCell(x, y, r, termbox.ColorDefault, termbox.ColorDefault)
-			x += 1
-		}
-		x = tempx
-		y += 1
-	}
-	// Because we always icrease it one more.
-	y -= 1
-	return x, y
-}
-
-func printTextNext(s string) (finalX, finalY int) {
-	line = line + 1
-	return printText(0, line, s)
 }
 
 // 'a' = 97		-> visual imagination
